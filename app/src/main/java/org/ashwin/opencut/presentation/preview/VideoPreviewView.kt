0d11da1f569aa93eab8c4ec302d420c5ad6c312d
Ashwin M <ashwinnambiarm@gmail.com>
2026-06-13 21:52:22 +0530

    refactor: migrate VideoPreviewView from Java to Kotlin

---
+++ app/src/main/java/org/ashwin/opencut/presentation/preview/VideoPreviewView.kt
@@ -0,0 +1,26 @@
+package org.ashwin.opencut.presentation.preview
+
+import android.content.Context
+import android.net.Uri
+import android.opengl.GLSurfaceView
+import org.ashwin.opencut.data.media.PreviewRenderer
+import org.ashwin.opencut.domain.model.EffectSettings
+
+class VideoPreviewView(context: Context) : GLSurfaceView(context) {
+
+    private val renderer: PreviewRenderer = PreviewRenderer(context)
+
+    init {
+        setEGLContextClientVersion(2)
+        setRenderer(renderer)
+        renderMode = RENDERMODE_CONTINUOUSLY
+    }
+
+    fun loadVideo(uri: Uri?) {
+        renderer.loadVideo(uri)
+    }
+
+    fun setEffect(effectSettings: EffectSettings) {
+        renderer.setEffect(effectSettings)
+    }
+}
