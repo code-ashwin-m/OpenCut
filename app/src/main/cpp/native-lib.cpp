98fdeffd1682316c135a63fb7a8d5898e88d05b0
Ashwin M <ashwinnambiarm@gmail.com>
2026-06-15 20:12:47 +0530

    empty project

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,28 +1,28 @@
 #include <jni.h>
 
-#include "core/OpenCutEngine.h"
+//#include "core/OpenCutEngine.h"
 
-namespace {
-opencut::OpenCutEngine engine;
-}
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_org_ashwin_opencut_platform_nativebridge_NativeEngineBridge_nativeEngineInfo(
-        JNIEnv* env,
-        jobject /* this */) {
-    return env->NewStringUTF(engine.engineInfo().c_str());
-}
-
-extern "C" JNIEXPORT void JNICALL
-Java_org_ashwin_opencut_platform_nativebridge_NativeEngineBridge_nativePrepareEngine(
-        JNIEnv* /* env */,
-        jobject /* this */) {
-    engine.prepare();
-}
-
-extern "C" JNIEXPORT void JNICALL
-Java_org_ashwin_opencut_platform_nativebridge_NativeEngineBridge_nativeReleaseEngine(
-        JNIEnv* /* env */,
-        jobject /* this */) {
-    engine.release();
-}
+//namespace {
+//opencut::OpenCutEngine engine;
+//}
+//
+//extern "C" JNIEXPORT jstring JNICALL
+//Java_org_ashwin_opencut_platform_nativebridge_NativeEngineBridge_nativeEngineInfo(
+//        JNIEnv* env,
+//        jobject /* this */) {
+//    return env->NewStringUTF(engine.engineInfo().c_str());
+//}
+//
+//extern "C" JNIEXPORT void JNICALL
+//Java_org_ashwin_opencut_platform_nativebridge_NativeEngineBridge_nativePrepareEngine(
+//        JNIEnv* /* env */,
+//        jobject /* this */) {
+//    engine.prepare();
+//}
+//
+//extern "C" JNIEXPORT void JNICALL
+//Java_org_ashwin_opencut_platform_nativebridge_NativeEngineBridge_nativeReleaseEngine(
+//        JNIEnv* /* env */,
+//        jobject /* this */) {
+//    engine.release();
+//}
---
+++ app/src/main/java/org/ashwin/opencut/ui/EditorScreen.kt
@@ -0,0 +1,8 @@
+package org.ashwin.opencut.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EditorScreen(){
+
+}
